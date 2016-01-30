7cb8f1581d2766f631712676b81db97cf437deb4
chiemekae <chiemeka@umd.edu>
2016-01-30 04:23:49 -0500

    background worker fix

--- worker_src/Mineral_worker.c
+++ worker_src/Mineral_worker.c
@@ -1,111 +1,36 @@
-/*
- * main.c
- * Constructs a Window housing an output TextLayer to show data from 
- * either modes of operation of the accelerometer.
- */
+#include <pebble_worker.h>
 
-#include <pebble.h>
+#define ENABLESEARCH 0
 
-#define TAP_NOT_DATA true
-
-static Window *s_main_window;
-static TextLayer *s_output_layer;
-
-static void data_handler(AccelData *data, uint32_t num_samples) {
-  // Long lived buffer
-  static char s_buffer[128];
-
-  // Compose string of all data
-  snprintf(s_buffer, sizeof(s_buffer), 
-    "N X,Y,Z\n0 %d,%d,%d\n1 %d,%d,%d\n2 %d,%d,%d", 
-    data[0].x, data[0].y, data[0].z, 
-    data[1].x, data[1].y, data[1].z, 
-    data[2].x, data[2].y, data[2].z
-  );
-
-  //Show the data
-  text_layer_set_text(s_output_layer, s_buffer);
-}
+static int16_t enable_search = 0;
 
 static void tap_handler(AccelAxisType axis, int32_t direction) {
-  switch (axis) {
-  case ACCEL_AXIS_X:
-    if (direction > 0) {
-      text_layer_set_text(s_output_layer, "X axis positive.");
-    } else {
-      text_layer_set_text(s_output_layer, "X axis negative.");
-    }
-    break;
-  case ACCEL_AXIS_Y:
-    if (direction > 0) {
-      text_layer_set_text(s_output_layer, "Y axis positive.");
-    } else {
-      text_layer_set_text(s_output_layer, "Y axis negative.");
-    }
-    break;
-  case ACCEL_AXIS_Z:
-    if (direction > 0) {
-      text_layer_set_text(s_output_layer, "Z axis positive.");
-    } else {
-      text_layer_set_text(s_output_layer, "Z axis negative.");
-    }
-    break;
+  if (axis == ACCEL_AXIS_Y){
+	enable_search = 1;	
+	//vibes_short_pulse();
   }
 }
 
-static void main_window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect window_bounds = layer_get_bounds(window_layer);
+static void send_to_foreground() {
+  // Construct a data packet
+  AppWorkerMessage msg_data = {
+    .data0 = enable_search
+  };
 
-  // Create output TextLayer
-  s_output_layer = text_layer_create(GRect(5, 0, window_bounds.size.w - 10, window_bounds.size.h));
-  text_layer_set_font(s_output_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
-  text_layer_set_text(s_output_layer, "No data yet.");
-  text_layer_set_overflow_mode(s_output_layer, GTextOverflowModeWordWrap);
-  layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
+  app_worker_send_message(ENABLESEARCH, &msg_data);
+  enable_search = 0;
 }
 
-static void main_window_unload(Window *window) {
-  // Destroy output TextLayer
-  text_layer_destroy(s_output_layer);
+static void worker_init() {
+  accel_tap_service_subscribe(tap_handler);
 }
 
-static void init() {
-  // Create main Window
-  s_main_window = window_create();
-  window_set_window_handlers(s_main_window, (WindowHandlers) {
-    .load = main_window_load,
-    .unload = main_window_unload
-  });
-  window_stack_push(s_main_window, true);
-
-  // Use tap service? If not, use data service
-  if (TAP_NOT_DATA) {
-    // Subscribe to the accelerometer tap service
-    accel_tap_service_subscribe(tap_handler);
-  } else {
-    // Subscribe to the accelerometer data service
-    int num_samples = 3;
-    accel_data_service_subscribe(num_samples, data_handler);
-
-    // Choose update rate
-    accel_service_set_sampling_rate(ACCEL_SAMPLING_10HZ);
-  }
-}
-
-static void deinit() {
-  // Destroy main Window
-  window_destroy(s_main_window);
-
-  if (TAP_NOT_DATA) {
-    accel_tap_service_unsubscribe();
-  } else {
-    accel_data_service_unsubscribe();
-  }
+static void worker_deinit() {
+  accel_tap_service_unsubscribe();
 }
 
 int main(void) {
-  init();
-  app_event_loop();
-  deinit();
+  worker_init();
+  worker_event_loop();
+  worker_deinit();
 }
