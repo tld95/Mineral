db652f9d4b1912cad8e58cd4032429952abf61fa
Brian Liu <b_liu72@yahoo.com>
2016-01-30 14:08:43 -0500

    Added more google search features return first page of links

--- server/hello.py
+++ server/hello.py
@@ -1,18 +1,27 @@
 from flask import Flask
+import flask
 from flask import request
+from flask.ext.cors import CORS
+
+print "starting"
+
 import search
 import json
 
 app = Flask(__name__)
+CORS(app)
+
 print "Ready!"
 @app.route("/api", methods=['GET','POST'])
 def hello():
     query = request.args.get('q')
     print "searching " + query
     answer = search.search(query)
-    response = {};
-    response['answer'] = answer
-    return json.dumps(response);
+
+    resp = flask.Response(json.dumps(answer))
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Content-Type'] = 'application/json'
+    return resp
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
--- server/search.py
+++ server/search.py
@@ -5,7 +5,7 @@ import urllib
 
 display = Display(visible=0, size=(1024,768))
 display.start()
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
 
 
 
@@ -13,14 +13,50 @@ def search(query):
     params = urllib.urlencode({'q':query})
     url = 'https://www.google.com/search?' + params
     driver.get(url)
+    response = {};
     results = [];
+
     list =  driver.find_elements_by_css_selector('._uX')
     if not len(list) == 0:
         for el in list:
-            results.append(el.text)
-        return results
+             results.append(el.text)
     else:
-        return "No result"
+        list2 = driver.find_elements_by_css_selector('[data-dobid=dfn]')
+        if not len(list2) == 0:
+            for el in list2:
+                results.append(el.text)
+        else:
+            try:
+                temp = driver.find_element_by_id("wob_tm").text
+                print temp
+                weather = driver.find_element_by_id("wob_dc").text
+                print weather
+                loc = driver.find_element_by_id("wob_loc").text
+                print loc
+                results.append(temp + "\u00B0F\n" + weather + "\n" + loc)
+            except:
+                try:
+                    results.append(driver.find_element_by_id("cwos").text)
+                except:
+                    results.append("No result")
+    ans = results[0];
+    for i in range(1,len(results)):
+        if len(results[i]) > 0:
+            ans = ans + '\n' + results[i]
+
+    response['answer'] = ans
+    search_results = []
+    slist = driver.find_elements_by_css_selector('.g .r a')
+    if not len(slist) == 0:
+        for el in slist:
+            sres = {"title": el.text}
+            link = el.get_attribute('href')
+            sres['url'] = link
+            search_results.append(sres)
+    else:
+        search_results = [{"title":"No results found"}]
+    response['searchResults'] = search_results
+    return response
 
 
 
